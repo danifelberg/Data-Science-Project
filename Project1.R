31fd3408824d470810091ed85922cced522d695b
sdf5y <122656037+sdf5y@users.noreply.github.com>
2023-02-15 19:01:24 -0500

    Add files via upload

--- Project1.R
+++ Project1.R
@@ -41,6 +41,7 @@ RECS2015$DOLELSPH <- currency(RECS2015$DOLELSPH,
                               format = "f",
                               big.mark = ",",
                               sep = "")
+svytotal(~DOLELSPH, des)
 
 #Electricity costs for air conditioning (“DOLELCOL” variable),
 
@@ -50,6 +51,7 @@ RECS2015$DOLELCOL <- currency(RECS2015$DOLELCOL,
                               format = "f",
                               big.mark = ",",
                               sep = "")
+svytotal(~DOLELCOL, des)
 
 #Electricity costs for water heating (“DOLELWTH” variable),
 
@@ -59,7 +61,7 @@ RECS2015$DOLELWTH <- currency(RECS2015$DOLELWTH,
                               format = "f",
                               big.mark = ",",
                               sep = "")
-
+svytotal(~DOLELWTH, des)
 #Electricity costs for all refrigerators (“DOLELRFG” variable),
 
 RECS2015$DOLELRFG <- currency(RECS2015$DOLELRFG,
@@ -68,18 +70,39 @@ RECS2015$DOLELRFG <- currency(RECS2015$DOLELRFG,
                               format = "f",
                               big.mark = ",",
                               sep = "")
+svytotal(~DOLELRFG, des)
+#Annual gross household income for the last year (“MONEYPY” variable),
 
-#Annual gross household income for the last year (“MONEPY” variable),
-
+RECS2015$MONEYPY <- as.factor(RECS2015$MONEYPY)
 
 #Electricity cost differences between urban and rural areas (“UATYP10” variable),
 
+#rename levels of area 
+RECS2015 <- RECS2015 %>% 
+  mutate(UATYP10 = as.factor(recode(UATYP10,
+                          U = "Urban Area",
+                          R = "Rural",
+                          C = "Urban Cluster")))
+
+#dataframe of area and total energy
+Tot_Energy_area_df <- data.frame(RECS2015$UATYP10, RECS2015$DOLLAREL, RECS2015$DIVISION)
+
+#rough plot to see expenditure differences by density classification
+plot(y = Tot_Energy_area_df$RECS2015.DOLLAREL,
+     x = Tot_Energy_area_df$RECS2015.UATYP10)
 
 #What are the energy costs (“DOLLAREL” variable) for homeowners based on the number of rooms (“TOTROOMS” variable)?
 
+house_size <- data.frame(RECS2015$DOLLAREL, RECS2015$TOTROOMS, RECS2015$TOTSQFT_EN)
+
+#rough plot for total squarefootage (x), and energy costs (y)
+plot(x = house_size$RECS2015.TOTSQFT_EN,
+     y = house_size$RECS2015.DOLLAREL)
 
 
 #Is Electric heating and cooling costs respective to certain climates (“CLIMATE_REGION_PUB”)?
+
+
 
 #----Mapping the data
 #relevant code for us to get started
